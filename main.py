640656e406f63ee689c13d37ffc4ddf75864a5ea
JustIItian <Bhuvikumar8143@gmail.com>
2025-05-19 12:18:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def show_random_emojis(message):
     return emoji_message
     
 # Define the owner's user ID
-OWNER_ID = 8172163893 # Replace with the actual owner's user ID
+OWNER_ID = 8172163893,6323515385 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
 SUDO_USERS = [8172163893,6323515385,-1002610747504]
